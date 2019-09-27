711686db6160c37adcd9f01f0ac83073313649c0
GLHolmstroff <23653416+GLHolmstroff@users.noreply.github.com>
2019-09-27 11:55:49 +0200

    Project setup

---
+++ Huishoud/src/main/kotlin/com/group2projc/Huishoud/HuishoudApplication.kt
@@ -0,0 +1,11 @@
+package com.group2projc.Huishoud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HuishoudApplication
+
+fun main(args: Array<String>) {
+	runApplication<HuishoudApplication>(*args)
+}
