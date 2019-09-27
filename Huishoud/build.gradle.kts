22328c2739493efde6ff683bce53097af1e02f54
QuintenVerheij <23653416+QuintenVerheij@users.noreply.github.com>
2019-09-27 12:37:35 +0200

    Update build.gradle.kts
    Added JUnit platform dependency

--- Huishoud/build.gradle.kts
+++ Huishoud/build.gradle.kts
@@ -11,15 +11,19 @@ group = "com.group2projc"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
-repositories {
-	mavenCentral()
-}
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	{
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
