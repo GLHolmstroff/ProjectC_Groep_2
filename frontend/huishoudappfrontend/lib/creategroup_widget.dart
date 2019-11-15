af822dc6c169fada04ddf87a00307e4514ea069a
EduardTer <45460962+EduardTer@users.noreply.github.com>
2019-11-15 17:17:21 +0100

    Make an http request for making a new groep

--- frontend/huishoudappfrontend/lib/creategroup_widget.dart
+++ frontend/huishoudappfrontend/lib/creategroup_widget.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:huishoudappfrontend/setup/auth.dart';
+
+import 'Objects.dart';
 
 class Creategroup_widget extends StatefulWidget {
   static String tag = "Creategroup_widget";
@@ -6,17 +10,31 @@ class Creategroup_widget extends StatefulWidget {
 }
 
 class _Creategroup_widget extends State {
-  void _makeGroup() {}
+  final _groupnameController = TextEditingController();
+  Future<void> _makeGroup() async {
+    String groupname = _groupnameController.text;
+    if (groupname.length > 4) {
+      String uid = await Auth().currentUser();
+      
+      final response = await get("http://10.0.2.2:8080/createGroup?name=$groupname&uid=$uid");
+      if (response.statusCode == 200) {
+        print("Succesfully Registered");
+      } else {
+        print("Connection Failed");
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final titleText = Text(
       "Groep aanmaken",
-      textAlign: TextAlign.center,
-    );
+      textAlign: TextAlign.left,
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold));
 
     final groupName = TextFormField(
       keyboardType: TextInputType.text,
+      controller: _groupnameController,
       decoration: InputDecoration(
         hintText: 'Groepsnaam',
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
@@ -34,62 +52,26 @@ class _Creategroup_widget extends State {
       child: Text('Uitnodigen en verder', style: TextStyle(fontSize: 20)),
     );
 
-    // return Scaffold(
-    //   body: Center(
-    //       child: Column(
-    //     mainAxisAlignment: MainAxisAlignment.center,
-    //     children: <Widget>[
-    //       Row(
-    //         mainAxisAlignment: MainAxisAlignment.center,
-    //         children: <Widget>[
-    //           Padding(
-    //             padding: EdgeInsets.all(20),
-    //             child: titleText,
-    //           )
-    //         ],
-    //       ),
-    //       Row(
-    //         mainAxisAlignment: MainAxisAlignment.center,
-    //         children: <Widget>[
-    //           Padding(
-    //             padding: EdgeInsets.all(20),
-    //             child: groupName,
-    //           )
-    //         ],
-    //       ),
-    //       Row(
-    //         mainAxisAlignment: MainAxisAlignment.center,
-    //         children: <Widget>[
-    //           Padding(
-    //             padding: EdgeInsets.all(20),
-    //             child: groepsledenText,
-    //           )
-    //         ],
-    //       ),
-    //       Row(
-    //         mainAxisAlignment: MainAxisAlignment.center,
-    //         children: <Widget>[
-    //           Padding(
-    //             padding: EdgeInsets.all(20),
-    //             child: makeGroupButton,
-    //           )
-    //         ],
-    //       )
-    //     ],
-    //   )),
-    // );
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(top:40.0, left: 8.0),
+          child: Container(
+            height: 80.0,
             child: titleText,
           ),
-          Container(child: groepsledenText,),
-          Container(child: makeGroupButton,)
-        ],
-      )
-    );
+        ),
+        Container(child: groupName),
+        Container(
+          child: groepsledenText,
+        ),
+        Container(
+          child: makeGroupButton,
+        )
+      ],
+    ));
   }
 }
