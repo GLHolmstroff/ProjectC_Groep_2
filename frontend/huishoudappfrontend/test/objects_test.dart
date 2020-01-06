96be0aa8d811d346ae5941e1ce331c39c09f902d
Quinten <Q.verheij@hotmail.com>
2020-01-06 21:48:19 +0100

    User and House tested

--- frontend/huishoudappfrontend/test/objects_test.dart
+++ frontend/huishoudappfrontend/test/objects_test.dart
@@ -21,6 +21,10 @@ class MockCurrentUser extends Mock implements CurrentUser {
   }
 }
 
+class MockUser extends Mock implements User {
+  
+}
+
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   group('CurrentUser', () {
@@ -79,5 +83,60 @@ void main() {
       expect(user.toString(),
           "userID: JdQOZfDU0fclLWzqjMCfpp9m8hz2\ngroupID: 1\npermission: user\nName: Quinten\n");
     });
-  }, group('user', () {}));
+  });
+  group('User', () {
+    test('should be able to be constructed', () {
+      var user = User('001', 1, 'user', 'Henk', 'NaN', 'user');
+      expect(user.userId, '001');
+      expect(user.groupId, 1);
+      expect(user.globalPermissions, 'user');
+      expect(user.displayName, 'Henk');
+      expect(user.picture_link, 'NaN');
+      expect(user.group_permission, 'user');
+    });
+
+    test('should read in from json correctly', () {
+      Map<String, dynamic> json = {
+        "global_permissions": "user",
+        "uid": "JdQOZfDU0fclLWzqjMCfpp9m8hz2",
+        "groupid": 1,
+        "group_permissions": "groupAdmin",
+        "display_name": "Quinten",
+        "picture_link": "2019-12-0214,26,48,451629testfile.png"
+      };
+
+      User user = User.fromJson(json);
+      expect(user.globalPermissions, 'user');
+      expect(user.userId, 'JdQOZfDU0fclLWzqjMCfpp9m8hz2');
+      expect(user.groupId, 1);
+      expect(user.group_permission, 'groupAdmin');
+      expect(user.displayName, 'Quinten');
+      expect(user.picture_link, '2019-12-0214,26,48,451629testfile.png');
+    });
+
+
+  });
+
+  group('House',() {
+    test('should be constructed correctly', () {
+      House house = House(groupId: 1, createdAt: 'now', houseName: 'name');
+      expect(house.groupId, 1);
+      expect(house.createdAt, 'now');
+      expect(house.houseName, 'name');
+    });
+
+    test('should read in from json correctly', () {
+      Map<String, dynamic> json = {
+        "created_at": "now",
+        "name": "name",
+        "groupid": 1,
+        
+      };
+
+      House house = House.fromJson(json);
+      expect(house.groupId, 1);
+      expect(house.createdAt, 'now');
+      expect(house.houseName, 'name');
+    });
+  });
 }
